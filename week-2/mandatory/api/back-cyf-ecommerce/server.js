2020571cbeb87ac75688122f821882e337887c35
leoncan122 <leon.can122@gmail.com>
2021-07-03 12:48:24 +0200

    until put modification of customer

--- week-2/mandatory/api/back-cyf-ecommerce/server.js
+++ week-2/mandatory/api/back-cyf-ecommerce/server.js
@@ -22,15 +22,21 @@ const selectProdByName = `SELECT p.*, supplier_name
 FROM products p JOIN suppliers s ON p.supplier_id = s.id
 WHERE p.product_name = $1`
 
-const selectCustomerById = `SELECT * FROM customers WHERE id = $1`
 
-const selectProducts = `select p.product_name , s.supplier_name 
-    from suppliers s inner join products p on p.supplier_id = s.id 
-    where p.id = $1`
+
+const selectProducts = `SELECT p.product_name , s.supplier_name 
+    FROM suppliers s INNER JOIN products p on p.supplier_id = s.id 
+    WHERE p.id = $1`
 ;
-
+const selectCustomerById = `SELECT * FROM customers WHERE id = $1`
 const selectedCustomer = `SELECT name FROM customers WHERE name = $1`
 const createCustomer = `INSERT INTO customers (name, address, city, country) VALUES ($1, $2, $3, $4)`
+const updateCustomer = `UPDATE customers SET name = $1, address = $2, city = $3, country = $4 WHERE id = $5`
+
+const selectSupplier = `SELECT * FROM suppliers WHERE id = $1`
+const createProduct = `INSERT INTO products (product_name, unit_price, supplier_id) VALUES ($1, $2, $3)`
+
+const createOrder = `INSERT INTO orders (order_date, order_reference, customer_id) VALUES ($1, $2, $3)`
 
 app.use(express.json());
 
@@ -90,6 +96,59 @@ app.post('/customers', function (req, res) {
         
     })
 })
+app.post('/customers/:customerId/orders', function (req, res) {
+    let customer_id = parseInt(req.params.customerId)
+    let {order_date, order_reference} = req.body
+    let values = [order_date, order_reference, customer_id]
+
+    if (!order_date || !order_reference || !customer_id) {
+        return res.send('Order is invalid')
+    }
+    pool.connect( (err, client, release) => {
+        if (err) {
+            res.send('Error acquiring client')
+        };
+        client.query(selectCustomerById, [customer_id], (err, result) => {
+            if(result.rowCount < 1) {
+                res.send('This customer does not exists')
+            } else {
+                client.query(createOrder, values, (err, result) => {
+                    release
+                    if (err) {
+                        res.send(err.message)
+                    }
+                    res.status(201).send(`The order was realized, please check your email`)
+                })
+            }
+            
+        })
+        
+    })
+
+})
+app.put('/customers/:customerId', function (req, res) {
+    let customer_id = parseInt(req.params.customerId)
+    let {name, address, city, country} = req.body
+    let values = [name, address, city, country, customer_id]  
+
+    pool.connect((err, client, release) => {
+        if (err) {
+            res.send('Error acquiring client')
+        }
+        client.query(selectCustomerById, [customer_id], (err, result) => {
+            if (err) {
+                res.send('Error excecuting query')
+            }
+            if (result.rowCount < 1) {
+                res.send('The customer with this id does not exist')
+            }
+            client.query(updateCustomer, values, (err, result) => {
+                res.status(201).send('The customer was updated')
+            })
+        })
+    })
+
+})
 app.get('/suppliers', function (req, res) {
     pool.query('SELECT * FROM suppliers', (error, result) => {
         res.status(200).json(result.rows)
@@ -120,6 +179,37 @@ app.get("/products/:product_id", async (request, response) => {
             response.status(400).send("input no valid"); 
     }
 });
+app.post('/products', function (req, res) {
+    let {product_name, unit_price, supplier_id} = req.body
+    let values = [product_name, unit_price, supplier_id]
+    //como verifico que ese producto no existe por el mismo supplier?
+    pool.connect( (err, client, release) => {
+        if (err) {
+            return res.send('Error acquiring client')
+        }
+        client.query(selectSupplier, [supplier_id], (err, result) => {
+            if(err) {
+                return res.status(500).send(err.message)
+            }
+            if( result.rowCount < 1) {
+                return res.send('The supplier does not exist yet, please insert new supplier')
+            }
+            if (Number(unit_price) === NaN) {
+                return res.send('Unit price Must be a integer')
+            }
+            client.query(createProduct, values, (err, result) => {
+                release
+                if (err) {
+                    
+                    res.send('Error excecuting query')
+                }
+                console.log(result.rows)
+                res.status(201).send(`Product  was created`)
+            })
+        })
+    })
+
+})
 
 
 
