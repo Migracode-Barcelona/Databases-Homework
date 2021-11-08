da72ce2af29e764d34211b50135709f3ba7a2770
Francisco Rodriguez Belluati <franrobelluati@gmail.com>
2021-11-08 13:43:49 +0100

    stretch goal

--- cyf-ecommerce-api/server.js
+++ cyf-ecommerce-api/server.js
@@ -17,10 +17,18 @@ app.get("/customers", function(req, res) {
 });
 
 app.get("/suppliers", function(req, res) {
-    pool.query('SELECT * FROM suppliersç', (error, result) => {
+    pool.query('SELECT * FROM suppliers', (error, result) => {
         res.json(result.rows);
     });
 });
+// (STRETCH GOAL) Add a new GET endpoint /products to load all the product names along with their supplier names.
+app.get("/products&suppliers", (req, res)=>{
+    pool.query(
+        'select product_name, supplier_name from products join suppliers on suppliers.id=products.supplier_id order by supplier_name', 
+        (error, result)=>{
+            res.json(result.rows)
+        })
+})
 
 app.listen(3000, function() {
     console.log("Server is listening on port 3000. Ready to accept requests!");
