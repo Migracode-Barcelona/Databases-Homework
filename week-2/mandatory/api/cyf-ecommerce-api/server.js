c38b16a5444888bed2a8a08ea24769e927ba32d7
gabiarno <gabiarno@hotmail.com>
2022-07-02 18:29:16 +0200

    homework

--- week-2/mandatory/api/cyf-ecommerce-api/server.js
+++ week-2/mandatory/api/cyf-ecommerce-api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { Pool } = require("pg");
+const { response } = require("express");
 const app = express();
 var corsOptions = {
   origin: "http://localhost:8081"
@@ -43,12 +44,15 @@ app.get("/suppliers", (req, res) => {
 
 // route to products
 app.get("/products", (req, res) => {
-    pool.query('select p.product_name, s.supplier_name  from products p join suppliers s on p.supplier_id = s.id', (error, response) => {
+   /* pool.query('select p.product_name, s.supplier_name  from products p join suppliers s on p.supplier_id = s.id', (error, response) => {
         if (error){
             console.log("Something went wrong" + error);
         }
         res.json(response);
-    });
+    });*/
+    pool.query('select p.product_name, s.supplier_name  from products p join suppliers s on p.supplier_id = s.id')
+    .then(response => res.json(response.rows))
+    .catch(error => console.log("Something went wrong" + error));
 });
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
